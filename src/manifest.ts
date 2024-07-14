8d5f09be1ff0f3054e77a190e2ab9de9a0c1fe06
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2024-07-14 12:16:49 +0200

    chore: update template

--- src/manifest.ts
+++ src/manifest.ts
@@ -39,6 +39,7 @@ export async function getManifest() {
       'storage',
       'activeTab',
       'contextMenus',
+      'sidePanel',
     ],
     host_permissions: ['*://*/*'],
     content_scripts: [
@@ -63,6 +64,19 @@ export async function getManifest() {
         ? `script-src \'self\' http://localhost:${port}; object-src \'self\'`
         : 'script-src \'self\'; object-src \'self\'',
     },
+  }
+
+  // add sidepanel
+  if (isFirefox) {
+    manifest.sidebar_action = {
+      default_panel: 'dist/sidepanel/index.html',
+    }
+  }
+  else {
+    // the sidebar_action does not work for chromium based
+    manifest.side_panel = {
+      default_path: 'dist/sidepanel/index.html',
+    }
   }
 
   // FIXME: not work in MV3
