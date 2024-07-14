1ad5f143b3f5684356927389371ecd668bb06b43
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2024-07-14 13:05:44 +0200

    feat: Add side panel #11

--- src/sidepanel/main.ts
+++ src/sidepanel/main.ts
@@ -1,8 +1,19 @@
 import { createApp } from 'vue'
-import App from './Sidepanel.vue'
-import { setupApp } from '~/logic/common-setup'
 import '../styles'
 
-const app = createApp(App)
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+import App from './Sidepanel.vue'
+import { setupApp } from '~/logic/common-setup'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+const app = createApp(App).use(vuetify)
 setupApp(app)
 app.mount('#app')
