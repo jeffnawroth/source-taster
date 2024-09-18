3700bf0099240858cb5d2f467c6e2eb37648101a
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2024-09-18 12:39:08 +0200

    feat: add network error state (wip) #18

---
+++ src/popup/NetworkErrorState.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const isOnline = ref(navigator.onLine)
+
+function updateOnlineStatus() {
+  isOnline.value = navigator.onLine
+}
+
+window.addEventListener('offline', () => {
+  updateOnlineStatus()
+})
+
+window.addEventListener('online', () => {
+  updateOnlineStatus()
+})
+</script>
+
+<template>
+  <v-empty-state
+    v-if="!isOnline"
+    action-text="Retry Request"
+    image="https://cdn.vuetifyjs.com/docs/images/components/v-empty-state/connection.svg"
+    text="There might be a problem with your connection. Please check your internet connection."
+    title="Something Went Wrong"
+  />
+</template>
