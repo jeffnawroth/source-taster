eccc565a76bf2e3efab5611fe355aca70d688343
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2024-10-23 15:03:57 +0200

    feat: add options categories

--- src/options/Options.vue
+++ src/options/Options.vue
@@ -57,80 +57,104 @@ watchEffect(() => {
     <AppBar />
     <v-main>
       <v-container>
-        <v-card flat>
-          <v-list
-            lines="two"
-            subheader
-          >
-            <v-list-subheader>{{ t('general') }}</v-list-subheader>
+        <v-row>
+          <v-col cols="12">
+            <v-card flat>
+              <v-list
+                lines="two"
+                subheader
+              >
+                <!-- Allgemeine Einstellungen -->
+                <v-list-subheader>{{ t('general') }}</v-list-subheader>
 
-            <v-list-item
-              :subtitle="t('auto-import-dois-description')"
-              :title="t('auto-import-dois')"
-              @click="autoImportOption = !autoImportOption"
-            >
-              <template #prepend>
-                <v-list-item-action start>
-                  <v-checkbox-btn
-                    v-model="autoImportOption"
-                    color="primary"
-                  />
-                </v-list-item-action>
-              </template>
-            </v-list-item>
+                <v-list-item
+                  :subtitle="t('auto-import-dois-description')"
+                  :title="t('auto-import-dois')"
+                  @click="autoImportOption = !autoImportOption"
+                >
+                  <template #prepend>
+                    <v-list-item-action start>
+                      <v-checkbox-btn
+                        v-model="autoImportOption"
+                        color="primary"
+                      />
+                    </v-list-item-action>
+                  </template>
+                </v-list-item>
+              </v-list>
+            </v-card>
+          </v-col>
+          <v-col cols="12">
+            <v-card>
+              <v-list>
+                <!-- Darstellungseinstellungen -->
+                <v-list-subheader>{{ t('display') }}</v-list-subheader>
 
-            <v-list-item
-              :title="t('use-popup')"
-              :subtitle="t('use-popup-description')"
-            >
-              <template #prepend>
-                <v-list-item-action start>
-                  <v-checkbox-btn
-                    v-model="isPopup"
-                    color="primary"
-                    @change="saveDisplayOption"
-                  />
-                </v-list-item-action>
-              </template>
-            </v-list-item>
+                <v-list-item
+                  :title="t('use-popup')"
+                  :subtitle="t('use-popup-description')"
+                >
+                  <template #prepend>
+                    <v-list-item-action start>
+                      <v-checkbox-btn
+                        v-model="isPopup"
+                        color="primary"
+                        @change="saveDisplayOption"
+                      />
+                    </v-list-item-action>
+                  </template>
+                </v-list-item>
+                <v-list-item
+                  :title="t('theme')"
+                  :subtitle="t('theme-option-description')"
+                >
+                  <template #prepend>
+                    <v-list-item-action start>
+                      <v-select
+                        v-model="themeOption"
+                        :items="themes"
+                        :item-title="(option) => t(option)"
+                        :item-value="(option) => option"
+                        density="compact"
+                        hide-details
+                        variant="solo"
+                        flat
+                      />
+                    </v-list-item-action>
+                  </template>
+                </v-list-item>
+              </v-list>
+            </v-card>
+          </v-col>
+          <v-col cols="12">
+            <v-card>
+              <v-list>
+                <!-- Spracheinstellungen -->
+                <v-list-subheader>{{ t('language') }}</v-list-subheader>
 
-            <v-list-item
-              :title="t('language')"
-              :subtitle="t('language-option-description')"
-            >
-              <template #prepend>
-                <v-list-item-action start>
-                  <v-select
-                    v-model="localeOption"
-                    :items="languages"
-                    :item-title="(option) => t(option.name.toLocaleLowerCase())"
-                    item-value="locale"
-                    hide-details
-                    density="compact"
-                  />
-                </v-list-item-action>
-              </template>
-            </v-list-item>
-
-            <v-list-item
-              :title="t('theme')"
-              :subtitle="t('theme-option-description')"
-            >
-              <template #prepend>
-                <v-list-item-action start>
-                  <v-select
-                    v-model="themeOption"
-                    :items="themes"
-                    :item-title="(option) => t(option)"
-                    :item-value="(option) => option"
-                    density="compact"
-                    hide-details
-                  />
-                </v-list-item-action>
-              </template>
-            </v-list-item>
-          </v-list>
-        </v-card>
+                <v-list-item
+                  :title="t('language')"
+                  :subtitle="t('language-option-description')"
+                >
+                  <template #prepend>
+                    <v-list-item-action start>
+                      <v-select
+                        v-model="localeOption"
+                        :items="languages"
+                        :item-title="(option) => t(option.name.toLocaleLowerCase())"
+                        item-value="locale"
+                        hide-details
+                        density="compact"
+                        variant="solo"
+                        flat
+                      />
+                    </v-list-item-action>
+                  </template>
+                </v-list-item>
+              </v-list>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-container>
     </v-main>
   </v-app>
