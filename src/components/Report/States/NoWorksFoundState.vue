d9947939d9ab6f26a5d91546973816a3a46ff9df
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2024-10-25 14:22:41 +0200

    fix: update state

--- src/components/Report/States/NoWorksFoundState.vue
+++ src/components/Report/States/NoWorksFoundState.vue
@@ -1,7 +1,25 @@
+<script setup lang="ts">
+import { useDoiStore } from '~/stores/doi'
+
+// I18n
+const { t } = useI18n()
+
+// Doi Store
+const { dois, bibliography } = storeToRefs(useDoiStore())
+</script>
+
 <template>
   <v-empty-state
-    icon="mdi-magnify"
-    :text="$t('no-works-found')"
-    :title="$t('no-match')"
+    v-if="dois.length === 0 && bibliography.length === 0"
+    icon="mdi-text-box-outline"
+    :text="t('enter-dois')"
+    :title="t('no-input-provided')"
+  />
+
+  <v-empty-state
+    v-else-if="dois.length === 0"
+    icon=" mdi-alert-circle-outline"
+    :title="t('no-valid-dois-were-found')"
+    :text="t('check-dois')"
   />
 </template>
