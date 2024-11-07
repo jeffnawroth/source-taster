18bc8ba3172be5cfd8ff8d18fc3b4fdb21cb7b0f
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2024-11-07 13:23:33 +0100

    fix(i18n): localization for context menu items

--- shim.d.ts
+++ shim.d.ts
@@ -1,6 +1,3 @@
-// eslint-disable-next-line unused-imports/no-unused-imports
-import type { ProtocolWithReturn } from 'webext-bridge'
-
 declare module 'webext-bridge' {
   export interface ProtocolMap {
     // define message protocol types
@@ -8,5 +5,6 @@ declare module 'webext-bridge' {
     bibliography: { selectedText: string }
     autoImportBibliography: { selectedText: string, url: string, type: string }
     autoImportPDFText: { url: string, type: string }
+    updateContextMenuWithLanguage: { locale: string }
   }
 }
