69c7dc75262c59f865212c22a6d894d77d651bc4
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2024-12-23 11:51:39 +0100

    refactor(states): refactor report states and translations

---
+++ src/components/Report/States/NoDoisFoundState.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { mdiAlertCircleOutline } from '@mdi/js'
+import { useDoiStore } from '~/stores/doi'
+
+// I18n
+const { t } = useI18n()
+
+// Doi Store
+const { dois, bibliography, file } = storeToRefs(useDoiStore())
+</script>
+
+<template>
+  <v-empty-state
+    v-if="dois.length === 0 && (!!bibliography.length || file)"
+    :icon=" mdiAlertCircleOutline"
+    :title="t('no-valid-dois-were-found')"
+    :text="t('check-dois')"
+  />
+</template>
--- src/components/Report/States/ReportState.vue
+++ src/components/Report/States/ReportState.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
-import type NetworkErrorState from './NetworkErrorState.vue'
-import { useDoiStore } from '~/stores/doi'
+// import type NetworkErrorState from './NetworkErrorState.vue'
+// import { useDoiStore } from '~/stores/doi'
 
-const { works, loading } = storeToRefs(useDoiStore())
+// const { works, loading } = storeToRefs(useDoiStore())
 
-type NetworkErrorStateType = InstanceType<typeof NetworkErrorState>
-const networkErrorStateRef = useTemplateRef<NetworkErrorStateType>('networkErrorStateRef')
+// type NetworkErrorStateType = InstanceType<typeof NetworkErrorState>
+// const networkErrorStateRef = useTemplateRef<NetworkErrorStateType>('networkErrorStateRef')
 </script>
 
 <template>
-  <NoWorksFoundState
+  <NoInputState />
+  <NoDoisFoundState />
+  <!-- <NoWorksFoundState
     v-show="works.length === 0 && !loading && networkErrorStateRef?.isOnline"
-  />
-  <NetworkErrorState
+  /> -->
+  <!-- <NetworkErrorState
     ref="networkErrorStateRef"
-  />
+  /> -->
 </template>
