1f70e557b5934ee67f1d256a4b1828cd244c452a
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2025-01-18 12:41:10 +0100

    feat: add AI usage tracking component and update localization for usage descriptions

---
+++ src/components/Options/OptionCategories/AISettings/AIUsageList.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+import { useIntervalFn } from '@vueuse/core'
+import { requestsMadeThisMinute, requestsMadeToday, tokensUsedThisMintue } from '~/logic'
+import { useAiStore } from '~/stores/ai'
+
+const { t } = useI18n()
+
+const aiStore = useAiStore()
+const { MAX_REQUESTS_PER_MINUTE, MAX_REQUEST_PER_DAY, MAX_TOKENS_PER_MINUTE } = storeToRefs(aiStore)
+
+const secondsUntilReset = ref(0)
+const hoursUntilReset = ref(0)
+
+useIntervalFn(() => {
+  const now = new Date()
+  secondsUntilReset.value = 60 - now.getSeconds()
+  if (secondsUntilReset.value === 60) {
+    requestsMadeThisMinute.value = 0
+    tokensUsedThisMintue.value = 0
+  }
+}, 1000)
+
+useIntervalFn(() => {
+  const now = new Date()
+  hoursUntilReset.value = 24 - now.getHours()
+  if (hoursUntilReset.value === 24) {
+    requestsMadeToday.value = 0
+  }
+}, 1000)
+</script>
+
+<template>
+  <v-list-item :title="t('usage')" />
+
+  <v-list-item
+    title="Request Per Day"
+  >
+    <template #subtitle>
+      <p>{{ t('requests-per-day-description') }}</p>
+    </template>
+    <p> {{ `${t('resets-in')} ${hoursUntilReset}` }}h </p>
+
+    <template #append>
+      <v-chip
+        color="primary"
+        label
+      >
+        {{ `${requestsMadeToday}/${MAX_REQUEST_PER_DAY}` }}
+      </v-chip>
+    </template>
+  </v-list-item>
+  <v-list-item
+    :title="t('requests-per-minute')"
+  >
+    <template #subtitle>
+      <p>{{ t('requests-per-minute-description') }}</p>
+    </template>
+    <p> {{ `${t('resets-in')} ${secondsUntilReset}` }}s </p>
+
+    <template #append>
+      <v-chip
+        color="primary"
+        label
+      >
+        {{ `${requestsMadeThisMinute}/${MAX_REQUESTS_PER_MINUTE}` }}
+      </v-chip>
+    </template>
+  </v-list-item>
+  <v-list-item
+    :title="t('tokens-per-minute')"
+  >
+    <template #subtitle>
+      <p>{{ t('tokens-per-minute-description') }}</p>
+    </template>
+    <p>
+      {{ `${t('resets-in')} ${secondsUntilReset}` }}s
+    </p>
+    <template #append>
+      <v-chip
+        color="primary"
+        label
+      >
+        {{ `${tokensUsedThisMintue}/${MAX_TOKENS_PER_MINUTE}` }}
+      </v-chip>
+    </template>
+  </v-list-item>
+</template>
--- src/components/Options/OptionCategories/AISettings/AiSettings.vue
+++ src/components/Options/OptionCategories/AISettings/AiSettings.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import { mdiInformationOutline, mdiKey, mdiStarFourPointsOutline } from '@mdi/js'
-import { useDebounceFn, useIntervalFn } from '@vueuse/core'
-import { aiExtractionOption, geminiApiKey, requestsMadeThisMinute, requestsMadeToday, tokensUsedThisMintue } from '~/logic'
+import { useDebounceFn } from '@vueuse/core'
+import { aiExtractionOption, geminiApiKey } from '~/logic'
 import { useAiStore } from '~/stores/ai'
+import AIUsageList from './AIUsageList.vue'
 
 // i18n
 const { t } = useI18n()
 
 const aiStore = useAiStore()
-const { apiKeyValid, loading, MAX_REQUESTS_PER_MINUTE, MAX_REQUEST_PER_DAY, MAX_TOKENS_PER_MINUTE } = storeToRefs(useAiStore())
+const { apiKeyValid, loading } = storeToRefs(useAiStore())
 const { testApiKey } = aiStore
 
 const show = ref(false)
@@ -34,26 +35,6 @@ watch(apiKeyValid, (newVal) => {
 
   showApiKeyStatusIcon()
 })
-
-const secondsUntilReset = ref(0)
-const hoursUntilReset = ref(0)
-
-useIntervalFn(() => {
-  const now = new Date()
-  secondsUntilReset.value = 60 - now.getSeconds()
-  if (secondsUntilReset.value === 60) {
-    requestsMadeThisMinute.value = 0
-    tokensUsedThisMintue.value = 0
-  }
-}, 1000)
-
-useIntervalFn(() => {
-  const now = new Date()
-  hoursUntilReset.value = 24 - now.getHours()
-  if (hoursUntilReset.value === 24) {
-    requestsMadeToday.value = 0
-  }
-}, 1000)
 
 function showApiKeyStatusIcon() {
   apiKeyStatusIcon.value = true
@@ -140,59 +121,7 @@ function showApiKeyStatusIcon() {
       </v-form>
     </v-list-item>
     <v-divider />
-    <v-list-item title="Usage" />
 
-    <v-list-item
-      title="Request Per Day"
-    >
-      <template #subtitle>
-        <p>This is the daily limit of AI requests</p>
-      </template>
-      <p>Resets in {{ hoursUntilReset }}h </p>
-
-      <template #append>
-        <v-chip
-          color="primary"
-          label
-        >
-          {{ `${requestsMadeToday}/${MAX_REQUEST_PER_DAY}` }}
-        </v-chip>
-      </template>
-    </v-list-item>
-    <v-list-item
-      title="Request Per Minute"
-    >
-      <template #subtitle>
-        <p>This is the minute limit of AI requests</p>
-      </template>
-      <p>Resets in {{ secondsUntilReset }}s </p>
-
-      <template #append>
-        <v-chip
-          color="primary"
-          label
-        >
-          {{ `${requestsMadeThisMinute}/${MAX_REQUESTS_PER_MINUTE}` }}
-        </v-chip>
-      </template>
-    </v-list-item>
-    <v-list-item
-      title="Tokens Per Minute"
-    >
-      <template #subtitle>
-        <p>This is the minute limit of AI tokens</p>
-      </template>
-      <p>
-        Resets in {{ secondsUntilReset }}s
-      </p>
-      <template #append>
-        <v-chip
-          color="primary"
-          label
-        >
-          {{ `${tokensUsedThisMintue}/${MAX_TOKENS_PER_MINUTE}` }}
-        </v-chip>
-      </template>
-    </v-list-item>
+    <AIUsageList />
   </OptionCategory>
 </template>
