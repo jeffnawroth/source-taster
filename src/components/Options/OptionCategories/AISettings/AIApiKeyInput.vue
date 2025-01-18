1c4d24b7be8386a11aad0d7a2d0fce3829611b88
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2025-01-18 14:24:22 +0100

    refactor: api key input

---
+++ src/components/Options/OptionCategories/AISettings/AIApiKeyInput.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+import { mdiKey } from '@mdi/js'
+import { useDebounceFn } from '@vueuse/core'
+import { geminiApiKey } from '~/logic'
+import { useAiStore } from '~/stores/ai'
+
+// I18n
+const { t } = useI18n()
+
+// AI Store
+const aiStore = useAiStore()
+const { apiKeyValid, loading } = storeToRefs(aiStore)
+const { testApiKey } = aiStore
+
+// Data
+const show = ref(false)
+
+const apiKeyStatusIcon = ref(false)
+
+// Watchers
+watch(apiKeyValid, (newVal) => {
+  if (newVal === null)
+    return
+
+  showApiKeyStatusIcon()
+})
+
+const handleTextInput = useDebounceFn(async () => {
+  await testApiKey()
+}, 500)
+
+watch(geminiApiKey, (newVal) => {
+  if (newVal.trim().length === 0) {
+    apiKeyValid.value = null
+  }
+  else {
+    handleTextInput()
+  }
+})
+
+// Methods
+function showApiKeyStatusIcon() {
+  apiKeyStatusIcon.value = true
+
+  setTimeout(() => {
+    apiKeyStatusIcon.value = false
+    apiKeyValid.value = null
+  }, 2000)
+}
+
+// Computed
+const type = computed(() => show.value ? 'text' : 'password')
+</script>
+
+<template>
+  <v-text-field
+    v-model="geminiApiKey"
+    variant="solo-filled"
+    placeholder="GEMINI_API_KEY"
+    flat
+    :prepend-inner-icon="mdiKey"
+    :messages="t('api-key-texfield-message')"
+    :type
+    clearable
+    @click:prepend-inner="show = !show"
+  >
+    <template #message="{ message }">
+      {{ message }}  <a
+        class="text-decoration-none"
+        href="https://aistudio.google.com/app/apikey"
+      >{{ t('create-one-here') }}</a>.
+    </template>
+
+    <template #append-inner>
+      <v-progress-circular
+        v-if="loading"
+        indeterminate
+        size="24"
+      />
+
+      <v-tooltip>
+        <template #activator="{ props }">
+          <v-icon
+            v-show="apiKeyStatusIcon"
+            v-bind="props"
+            :icon="apiKeyValid ? '$success' : ' $error'"
+            :color=" apiKeyValid ? 'success' : 'error'"
+          />
+        </template>
+        {{ apiKeyValid ? t('api-key-valid') : t('api-key-invalid') }}
+      </v-tooltip>
+    </template>
+  </v-text-field>
+</template>
--- src/components/Options/OptionCategories/AISettings/AiSettings.vue
+++ src/components/Options/OptionCategories/AISettings/AiSettings.vue
@@ -1,49 +1,10 @@
 <script setup lang="ts">
-import { mdiInformationOutline, mdiKey, mdiStarFourPointsOutline } from '@mdi/js'
-import { useDebounceFn } from '@vueuse/core'
+import { mdiInformationOutline, mdiStarFourPointsOutline } from '@mdi/js'
 import { aiExtractionOption, geminiApiKey } from '~/logic'
-import { useAiStore } from '~/stores/ai'
 import AIUsageList from './AIUsageList.vue'
 
 // i18n
 const { t } = useI18n()
-
-const aiStore = useAiStore()
-const { apiKeyValid, loading } = storeToRefs(useAiStore())
-const { testApiKey } = aiStore
-
-const show = ref(false)
-
-const apiKeyStatusIcon = ref(false)
-
-const handleTextInput = useDebounceFn(async () => {
-  await testApiKey()
-}, 500)
-
-watch(geminiApiKey, (newVal) => {
-  if (newVal.trim().length === 0) {
-    apiKeyValid.value = null
-  }
-  else {
-    handleTextInput()
-  }
-})
-
-watch(apiKeyValid, (newVal) => {
-  if (newVal === null)
-    return
-
-  showApiKeyStatusIcon()
-})
-
-function showApiKeyStatusIcon() {
-  apiKeyStatusIcon.value = true
-
-  setTimeout(() => {
-    apiKeyStatusIcon.value = false
-    apiKeyValid.value = null
-  }, 2000)
-}
 </script>
 
 <template>
@@ -79,46 +40,7 @@ function showApiKeyStatusIcon() {
       />
     </OptionListItem>
     <v-list-item>
-      <v-form>
-        <v-text-field
-          v-model="geminiApiKey"
-          variant="solo-filled"
-          placeholder="GEMINI_API_KEY"
-          flat
-          :prepend-inner-icon="mdiKey"
-          :messages="t('api-key-texfield-message')"
-          :type="show ? 'text' : 'password'"
-          clearable
-          @click:prepend-inner="show = !show"
-        >
-          <template #message="{ message }">
-            {{ message }}  <a
-              class="text-decoration-none"
-              href="https://aistudio.google.com/app/apikey"
-            >{{ t('create-one-here') }}</a>.
-          </template>
-
-          <template #append-inner>
-            <v-progress-circular
-              v-if="loading"
-              indeterminate
-              size="24"
-            />
-
-            <v-tooltip>
-              <template #activator="{ props }">
-                <v-icon
-                  v-show="apiKeyStatusIcon"
-                  v-bind="props"
-                  :icon="apiKeyValid ? '$success' : ' $error'"
-                  :color=" apiKeyValid ? 'success' : 'error'"
-                />
-              </template>
-              {{ apiKeyValid ? t('api-key-valid') : t('api-key-invalid') }}
-            </v-tooltip>
-          </template>
-        </v-text-field>
-      </v-form>
+      <AIApiKeyInput />
     </v-list-item>
     <v-divider />
 
