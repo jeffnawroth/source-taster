9d1091d0f323286b7476643bd7a8596a694c2c77
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2025-01-28 17:58:05 +0100

    refactor(router): update component imports to match new file names for options views

--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -1,9 +1,9 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 
 const routes = [
-  { path: '/', component: () => import('../options/views/GeneralSettings.vue'), name: 'general' },
-  { path: '/appearance', component: () => import('../options/views/DisplaySettings.vue'), name: 'appearance' },
-  { path: '/language', component: () => import('../options/views/LanguageSettings.vue'), name: 'language' },
+  { path: '/', component: () => import('../options/views/General.vue'), name: 'general' },
+  { path: '/appearance', component: () => import('../options/views/Appearance.vue'), name: 'appearance' },
+  { path: '/language', component: () => import('../options/views/Language.vue'), name: 'language' },
   { path: '/about', component: () => import('../options/views/About.vue'), name: 'about' },
   { path: '/help', component: () => import('../options/views/Help.vue'), name: 'help' },
 ]
