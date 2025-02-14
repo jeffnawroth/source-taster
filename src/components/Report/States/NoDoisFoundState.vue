fb99a40967bbd359b17e16c8730add7e706606a8
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2025-02-14 19:05:19 +0100

    fix(state): add loading state check to NoDoisFoundState component

--- src/components/Report/States/NoDoisFoundState.vue
+++ src/components/Report/States/NoDoisFoundState.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { mdiAlertCircleOutline } from '@mdi/js'
+import { useAppStore } from '~/stores/app'
 import { useDoiStore } from '~/stores/doi'
 import { useFileStore } from '~/stores/file'
 import { useTextStore } from '~/stores/text'
@@ -15,11 +16,14 @@ const { file } = storeToRefs(useFileStore())
 
 // TEXT
 const { text } = storeToRefs(useTextStore())
+
+// LOADING
+const { isLoading } = storeToRefs(useAppStore())
 </script>
 
 <template>
   <v-empty-state
-    v-if="extractedDois.length === 0 && (!!text.length || file)"
+    v-if="extractedDois.length === 0 && (!!text.length || file) && !isLoading"
     :icon=" mdiAlertCircleOutline"
     :title="t('no-valid-dois-were-found')"
     :text="t('check-dois')"
