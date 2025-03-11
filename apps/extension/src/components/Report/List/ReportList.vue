73117b1f7b75d8487a10060b63c0e79d35d8900f
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2025-03-11 11:47:01 +0100

    feat(extension): add fade transition to report list rendering #80

--- apps/extension/src/components/Report/List/ReportList.vue
+++ apps/extension/src/components/Report/List/ReportList.vue
@@ -6,15 +6,17 @@ const { works } = storeToRefs(useWorkStore())
 </script>
 
 <template>
-  <v-list
-    v-if="works.length"
-  >
-    <v-slide-y-transition group>
-      <ReportListItem
-        v-for="(work) in works"
-        :key="work.content?.message.DOI"
-        :work
-      />
-    </v-slide-y-transition>
-  </v-list>
+  <v-fade-transition>
+    <v-list
+      v-if="works.length"
+    >
+      <v-slide-y-transition group>
+        <ReportListItem
+          v-for="(work) in works"
+          :key="work.content?.message.DOI"
+          :work
+        />
+      </v-slide-y-transition>
+    </v-list>
+  </v-fade-transition>
 </template>
