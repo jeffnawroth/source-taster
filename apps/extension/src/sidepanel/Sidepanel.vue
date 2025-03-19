76306fcc490f06b707f08575fb2c6a7f3ccca8ad
Eren Cicek <ec_61@hotmail.de>
2025-03-19 21:12:19 +0100

    fix(reportlist): use dynamic height for scroll container #73

--- apps/extension/src/sidepanel/Sidepanel.vue
+++ apps/extension/src/sidepanel/Sidepanel.vue
@@ -4,7 +4,7 @@
   <v-app style="height: 100vh; overflow: hidden;">
     <AppBar />
     <v-main :scrollable="false">
-      <v-container>
+      <v-container style="max-height: calc(100vh - 64px - 20px); overflow: hidden; display: flex; flex: 1;">
         <AppCard />
       </v-container>
     </v-main>
