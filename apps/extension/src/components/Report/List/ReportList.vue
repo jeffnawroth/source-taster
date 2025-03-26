51dcb1edd94215cad6d42e5f19fa483e6d75e723
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2025-03-26 17:07:25 +0100

    fix(reportlist): simplify template structure by removing unnecessary transitions #80

--- apps/extension/src/components/Report/List/ReportList.vue
+++ apps/extension/src/components/Report/List/ReportList.vue
@@ -6,17 +6,15 @@ const { works } = storeToRefs(useWorkStore())
 </script>
 
 <template>
-  <v-fade-transition>
-    <v-list
-      v-if="works.length"
-    >
-      <v-slide-y-transition group>
-        <ReportListItem
-          v-for="(work) in works"
-          :key="work.content?.message.DOI"
-          :work
-        />
-      </v-slide-y-transition>
-    </v-list>
-  </v-fade-transition>
+  <v-list
+    v-if="works.length"
+  >
+    <v-slide-y-transition group>
+      <ReportListItem
+        v-for="(work) in works"
+        :key="work.content?.message.DOI"
+        :work
+      />
+    </v-slide-y-transition>
+  </v-list>
 </template>
