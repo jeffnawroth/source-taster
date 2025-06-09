ced921a2f4cc7197578de10731d5f85a1ee6748f
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2025-06-09 09:55:19 +0200

    feat: enable button disabling logic based on text and file presence

--- apps/extension/src/components/Input/CheckReferencesButton.vue
+++ apps/extension/src/components/Input/CheckReferencesButton.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import { useAppStore } from '@/extension/stores/app'
+import { useFileStore } from '@/extension/stores/file'
 import { useMetadataStore } from '@/extension/stores/metadata'
 import { useTextStore } from '@/extension/stores/text'
 
 const { extractAndSearchMetadata } = useMetadataStore()
+
+// DISABLE BUTTON IF NO TEXT AND FILE OR LOADING
 const { text } = storeToRefs(useTextStore())
+const { file } = storeToRefs(useFileStore())
+const { isLoading } = storeToRefs(useAppStore())
+
+const disabled = computed(() => (!file.value && !text.value.trim()) || isLoading.value)
 </script>
 
 <template>
@@ -11,6 +19,7 @@ const { text } = storeToRefs(useTextStore())
     variant="tonal"
     :text="$t('extract-and-verify')"
     block
+    :disabled
     @click="extractAndSearchMetadata(text)"
   />
 </template>
