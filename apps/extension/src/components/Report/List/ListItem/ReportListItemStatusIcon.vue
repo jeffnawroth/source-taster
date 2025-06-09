e8f5f83a9112f9b278c52ea38943ba584d9a6336
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2025-06-09 11:09:44 +0200

    feat: add tooltip functionality for verification status icon

--- apps/extension/src/components/Report/List/ListItem/ReportListItemStatusIcon.vue
+++ apps/extension/src/components/Report/List/ListItem/ReportListItemStatusIcon.vue
@@ -14,11 +14,26 @@ const icon = computed(() => {
   }
   return verification.match ? mdiCheckCircleOutline : mdiAlertCircleOutline
 })
+
+// ICON TOOLTIP TEXT
+const { t } = useI18n()
+
+const text = computed(() => {
+  if (!verification) {
+    return t('error')
+  }
+  return verification.match ? t('verified') : t('unverified')
+})
 </script>
 
 <template>
-  <v-icon
-    size="large"
-    :icon
-  />
+  <v-tooltip :text>
+    <template #activator="{ props }">
+      <v-icon
+        v-bind="props"
+        size="large"
+        :icon
+      />
+    </template>
+  </v-tooltip>
 </template>
