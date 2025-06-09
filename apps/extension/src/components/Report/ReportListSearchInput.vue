b14ca5b4d574b2d163132ebe7dc2695e2bc77f52
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2025-06-09 10:24:13 +0200

    feat: conditionally render search input based on verified references

--- apps/extension/src/components/Report/ReportListSearchInput.vue
+++ apps/extension/src/components/Report/ReportListSearchInput.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import { useMetadataStore } from '@/extension/stores/metadata'
+
 const search = defineModel({ default: '' })
+
+// SHOW SEARCH INPUT IF THERE ARE REFERENCES
+const { verifiedReferences } = storeToRefs(useMetadataStore())
+
+const show = computed(() => verifiedReferences.value.length)
 </script>
 
 <template>
   <v-text-field
+    v-if="show"
     v-model.trim="search"
     density="compact"
     hide-details
