c6469aaa1831f1b951a1208e47dca8d713593182
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2025-06-12 15:42:56 +0200

    refactor: move publication search logic to europePmcService

--- apps/extension/src/stores/europe-pmc.ts
+++ apps/extension/src/stores/europe-pmc.ts
@@ -1,51 +1,26 @@
-import type { EuropePmcPublicationsResponse, PublicationMetadata, ReferenceMetadata } from '../types'
+// src/stores/europePmcStore.ts
+
+import type { PublicationMetadata, ReferenceMetadata } from '../types'
 import { useMemoize } from '@vueuse/core'
 import { acceptHMRUpdate, defineStore } from 'pinia'
-import { Configuration, EuropePMCArticlesRESTfulAPIApi } from '../api/europe-pmc'
-import { mapEuropePMCToPublication } from '../utils/metadataMapper'
-
-const config = new Configuration({
-  basePath: 'https://www.ebi.ac.uk/europepmc/webservices/rest',
-})
+import { searchEuropePmcPublication } from '../services/europePmcService'
 
 export const useEuropePmcStore = defineStore('europe-pmc', () => {
-  const client = new EuropePMCArticlesRESTfulAPIApi(config)
-
-  /**
-   * Searches for a publication in the Europe PMC API using the provided reference metadata.
-   * @param referenceMetadata The metadata of the reference to search for.
-   * @returns The publication metadata if found, or null if not found.
-   */
   const searchPublication = useMemoize(
     async (referenceMetadata: ReferenceMetadata): Promise<PublicationMetadata | null> => {
-      if (!referenceMetadata.title) {
-        return null
-      }
-
       try {
-        const response = await client.search({
-          query: referenceMetadata.title,
-          pageSize: 1,
-          format: 'json',
-          email: import.meta.env.VITE_MAILTO,
-        }) as EuropePmcPublicationsResponse
-
-        if (!response.resultList?.result?.length) {
-          return null
-        }
-
-        const mappedResponse = mapEuropePMCToPublication(response.resultList?.result?.[0])
-        return mappedResponse
+        return await searchEuropePmcPublication(referenceMetadata)
       }
-
       catch (error) {
-        console.error('Europe PMC API Error:', error)
+        console.error('Europe PMC Store Error:', error)
         return null
       }
     },
   )
 
-  return { searchPublication }
+  return {
+    searchPublication,
+  }
 })
 
 if (import.meta.hot) {
