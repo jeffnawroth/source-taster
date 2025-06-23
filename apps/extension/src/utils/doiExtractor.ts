8628494fff67de58ae35ecb351006ff084277f5f
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2025-06-23 11:31:42 +0200

    refactor: comment out unused files

--- apps/extension/src/utils/doiExtractor.ts
+++ apps/extension/src/utils/doiExtractor.ts
@@ -1,45 +1,45 @@
-import type { ReferenceMetadata } from '../types'
+// import type { ReferenceMetadata } from '../types'
 
-// src/utils/doiExtractor.ts
-export function extractDois(text: string): ReferenceMetadata[] {
-  const extractedDOIs: ReferenceMetadata[] = []
+// // src/utils/doiExtractor.ts
+// export function extractDois(text: string): ReferenceMetadata[] {
+//   const extractedDOIs: ReferenceMetadata[] = []
 
-  const doiPatterns = [
-    /10\.\d{4,9}\/[-.\w;()/:]+/g, // Modern Crossref DOIs
-    /10\.1002\/\S+/g, // Early Crossref DOIs
-    // eslint-disable-next-line regexp/no-super-linear-backtracking, regexp/no-misleading-capturing-group, regexp/optimal-quantifier-concatenation
-    /10\.\d{4}\/\d+-\d+X?(\d+)\d+<\w+:\w*>\d+\.\d+\.\w+;\d/gi, // Rare DOI type 1
-    /10\.1021\/\w\w\d+/g, // Rare DOI type 2
-    /10\.1207\/\w+&\d+_\d+/g, // Rare DOI type 3
-  ]
+//   const doiPatterns = [
+//     /10\.\d{4,9}\/[-.\w;()/:]+/g, // Modern Crossref DOIs
+//     /10\.1002\/\S+/g, // Early Crossref DOIs
+//     // eslint-disable-next-line regexp/no-super-linear-backtracking, regexp/no-misleading-capturing-group, regexp/optimal-quantifier-concatenation
+//     /10\.\d{4}\/\d+-\d+X?(\d+)\d+<\w+:\w*>\d+\.\d+\.\w+;\d/gi, // Rare DOI type 1
+//     /10\.1021\/\w\w\d+/g, // Rare DOI type 2
+//     /10\.1207\/\w+&\d+_\d+/g, // Rare DOI type 3
+//   ]
 
-  // Extended pattern for URLs containing DOIs (global flag 'g' to match multiple occurrences)
-  const urlPattern = /https?:\/\/[\w.]+\/doi\/(10\.\d{4,9}\/[-.\w;()/:]+)/gi
+//   // Extended pattern for URLs containing DOIs (global flag 'g' to match multiple occurrences)
+//   const urlPattern = /https?:\/\/[\w.]+\/doi\/(10\.\d{4,9}\/[-.\w;()/:]+)/gi
 
-  // Check for embedded DOIs in URLs (using global flag 'g')
-  let urlMatches
-  // eslint-disable-next-line no-cond-assign
-  while ((urlMatches = urlPattern.exec(text)) !== null) {
-    extractedDOIs.push(
-      {
-        originalEntry: urlMatches[1].replace(/\.$/, ''),
-        doi: urlMatches[1].replace(/\.$/, ''),
-      },
-    ) // Wrap in ReferenceMetadata object
-  }
+//   // Check for embedded DOIs in URLs (using global flag 'g')
+//   let urlMatches
+//   // eslint-disable-next-line no-cond-assign
+//   while ((urlMatches = urlPattern.exec(text)) !== null) {
+//     extractedDOIs.push(
+//       {
+//         originalEntry: urlMatches[1].replace(/\.$/, ''),
+//         doi: urlMatches[1].replace(/\.$/, ''),
+//       },
+//     ) // Wrap in ReferenceMetadata object
+//   }
 
-  // Check for normal DOIs based on the defined patterns
-  doiPatterns.forEach((pattern) => {
-    const matches = text.match(pattern)
-    if (matches) {
-      extractedDOIs.push(
-        ...matches.map(match => ({
-          originalEntry: match.replace(/\.$/, ''),
-          doi: match.replace(/\.$/, ''), // Remove any trailing period if present
-        })), // Map to ReferenceMetadata objects
-      )
-    }
-  })
+//   // Check for normal DOIs based on the defined patterns
+//   doiPatterns.forEach((pattern) => {
+//     const matches = text.match(pattern)
+//     if (matches) {
+//       extractedDOIs.push(
+//         ...matches.map(match => ({
+//           originalEntry: match.replace(/\.$/, ''),
+//           doi: match.replace(/\.$/, ''), // Remove any trailing period if present
+//         })), // Map to ReferenceMetadata objects
+//       )
+//     }
+//   })
 
-  return extractedDOIs || []
-}
+//   return extractedDOIs || []
+// }
--- apps/extension/src/utils/issn-extractor.ts
+++ apps/extension/src/utils/issn-extractor.ts
@@ -1,19 +1,19 @@
-/**
- * Extracts all ISSNs from the provided text.
- * @param {string} text - The text from which to extract ISSNs.
- * @returns {string[]} An array of ISSN strings found in the text.
- */
-export default function extractIssns(text: string): string[] {
-  // Regular expression to match the ISSN pattern:
-  // \b      - word boundary ensures that we don't match extra characters
-  // \d{4}   - exactly four digits
-  // -       - a hyphen
-  // \d{3}   - exactly three digits
-  // [\dX]   - one digit or an "X"
-  // \b      - word boundary to finish the match cleanly
-  const regex = /\b\d{4}-\d{3}[\dX]\b/g
-  const matches = text.match(regex)
+// /**
+//  * Extracts all ISSNs from the provided text.
+//  * @param {string} text - The text from which to extract ISSNs.
+//  * @returns {string[]} An array of ISSN strings found in the text.
+//  */
+// export default function extractIssns(text: string): string[] {
+//   // Regular expression to match the ISSN pattern:
+//   // \b      - word boundary ensures that we don't match extra characters
+//   // \d{4}   - exactly four digits
+//   // -       - a hyphen
+//   // \d{3}   - exactly three digits
+//   // [\dX]   - one digit or an "X"
+//   // \b      - word boundary to finish the match cleanly
+//   const regex = /\b\d{4}-\d{3}[\dX]\b/g
+//   const matches = text.match(regex)
 
-  // Return unique ISSNs
-  return matches || []
-}
+//   // Return unique ISSNs
+//   return matches || []
+// }
