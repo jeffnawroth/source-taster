2f0bf52c6e4ce91b1d8be180b3d218b35d0627c3
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2025-06-24 14:09:37 +0200

    Remove unused Crossref API models and runtime code

--- apps/extension/src/api/crossref/apis/index.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './FundersApi';
-export * from './JournalsApi';
-export * from './LicensesApi';
-export * from './MembersApi';
-export * from './PrefixesApi';
-export * from './TypesApi';
-export * from './WorksApi';
