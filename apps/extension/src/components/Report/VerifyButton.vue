bba8af457db9454300aa4fa9dcac8e48f8c945b2
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2025-09-15 20:13:00 +0200

    fix: show always verify button

--- apps/extension/src/components/Report/VerifyButton.vue
+++ apps/extension/src/components/Report/VerifyButton.vue
@@ -1,49 +1,44 @@
 <script setup lang="ts">
 import { mdiMagnifyExpand } from '@mdi/js'
 import { useVerification } from '@/extension/composables/useVerification'
-import { settings } from '@/extension/logic'
-import { useAnystyleStore } from '@/extension/stores/anystyle'
 
 const { canVerify, isVerifying, verifyError, verify } = useVerification()
-const { hasParseResults } = storeToRefs(useAnystyleStore())
 </script>
 
 <template>
-  <div v-if="!settings.extract.useAi || hasParseResults">
-    <v-btn
-      :disabled="!canVerify || isVerifying"
-      color="success"
-      variant="tonal"
-      block
-      @click="verify"
-    >
-      <template #prepend>
-        <v-progress-circular
-          v-if="isVerifying"
-          size="20"
-          width="2"
-          indeterminate
-        />
-        <v-icon
-          v-else
-          :icon="mdiMagnifyExpand"
-          start
-        />
-      </template>
-      {{ isVerifying ? `${$t('verifying')}...` : $t('verify-references') }}
-    </v-btn>
+  <v-btn
+    :disabled="!canVerify || isVerifying"
+    color="success"
+    variant="tonal"
+    block
+    @click="verify"
+  >
+    <template #prepend>
+      <v-progress-circular
+        v-if="isVerifying"
+        size="20"
+        width="2"
+        indeterminate
+      />
+      <v-icon
+        v-else
+        :icon="mdiMagnifyExpand"
+        start
+      />
+    </template>
+    {{ isVerifying ? `${$t('verifying')}...` : $t('verify-references') }}
+  </v-btn>
 
-    <v-expand-transition>
-      <v-alert
-        v-if="verifyError"
-        type="error"
-        variant="tonal"
-        class="mt-3"
-        closable
-        @click:close="verifyError = null"
-      >
-        {{ verifyError }}
-      </v-alert>
-    </v-expand-transition>
-  </div>
+  <v-expand-transition>
+    <v-alert
+      v-if="verifyError"
+      type="error"
+      variant="tonal"
+      class="mt-3"
+      closable
+      @click:close="verifyError = null"
+    >
+      {{ verifyError }}
+    </v-alert>
+  </v-expand-transition>
 </template>
