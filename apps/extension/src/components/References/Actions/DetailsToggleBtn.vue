1bb0a89d4e19a3dc12db080cbf0936019e495c2b
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2025-09-19 19:55:02 +0200

    refactor: enhance DetailsToggleBtn component structure and tooltip functionality

--- apps/extension/src/components/References/Actions/DetailsToggleBtn.vue
+++ apps/extension/src/components/References/Actions/DetailsToggleBtn.vue
@@ -1,18 +1,29 @@
 <script setup lang="ts">
 import { mdiChevronDown, mdiChevronUp } from '@mdi/js'
+import { computed } from 'vue'
 
 const showDetails = defineModel({
   default: false,
 })
+
+const { t } = useI18n()
+
+const tooltipText = computed(() =>
+  showDetails.value ? t('hide-details') : t('show-details'),
+)
 </script>
 
 <template>
   <!-- Details Toggle -->
-  <v-icon
-    :icon="showDetails ? mdiChevronUp : mdiChevronDown"
-    class="cursor-pointer"
-    @click="showDetails = !showDetails"
-  >
-    <!-- {{ showDetails ? $t('hide-details') : $t('show-details') }} -->
-  </v-icon>
+  <v-tooltip location="bottom">
+    <template #activator="{ props: tooltipProps }">
+      <v-icon
+        :icon="showDetails ? mdiChevronUp : mdiChevronDown"
+        class="cursor-pointer"
+        v-bind="tooltipProps"
+        @click="showDetails = !showDetails"
+      />
+    </template>
+    {{ tooltipText }}
+  </v-tooltip>
 </template>
