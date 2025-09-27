d96c9b28901604522266aad22fde1997d5050d30
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2025-09-27 11:26:04 +0200

    refactor: replace chrome.storage with webextension-polyfill for storage operations

--- apps/extension/src/logic/sidepanelVisibilityListener.ts
+++ apps/extension/src/logic/sidepanelVisibilityListener.ts
@@ -1,14 +1,14 @@
+import { runtime } from 'webextension-polyfill'
+
 // sidepanelVisibilityListener.ts
 export function setupSidepanelVisibilityListener() {
   document.addEventListener('visibilitychange', () => {
     if (document.visibilityState === 'hidden') {
       // Send a message to the background process when the sidepanel is hidden or closed
-      // @ts-expect-error missing types
-      chrome.runtime.sendMessage({ type: 'SIDE_PANEL_CLOSED' })
+      void runtime.sendMessage({ type: 'SIDE_PANEL_CLOSED' })
     }
     else if (document.visibilityState === 'visible') {
-      // @ts-expect-error missing types
-      chrome.runtime.sendMessage({ type: 'SIDE_PANEL_OPENED' })
+      void runtime.sendMessage({ type: 'SIDE_PANEL_OPENED' })
     }
   })
 }
